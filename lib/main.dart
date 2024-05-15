e299b2ad3e552aabb04fb6508a79a62ca3f2f7fa
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-15 11:48:14 -0300

    Row e column

--- lib/main.dart
+++ lib/main.dart
@@ -16,19 +16,60 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              color: Colors.amber,
-              width: 100,
-              height: 100,
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: Colors.amber,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
             ),
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.amber,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            )
           ],
         ));
   }
