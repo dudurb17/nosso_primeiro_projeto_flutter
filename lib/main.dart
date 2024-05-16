2d7a8fd71a54c0a1e1b7485a3cce33c9db00581a
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-16 09:02:50 -0300

    criando uma class para nao repetir cod

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Tarefas"),
+            backgroundColor: Colors.blue[300],
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Task(nome: "Andar de bike"),
+              Task(nome: "Andar de carro"),
+              Task(nome: "Andar de aviao"),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {}),
+        ));
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task({super.key, required this.nome});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
       ),
-      home: 
     );
   }
 }
